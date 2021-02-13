2dbf12ab1a444c9e67af44281db502b84f209ef9
Farhan Hai Khan <njrfarhandasilva10@gmail.com>
2021-02-14 02:18:37 +0530

    Add Team Page

--- Blogs/app.py
+++ Blogs/app.py
@@ -6,7 +6,15 @@ ROOT_DIR = os.getcwd()
 
 @app.route('/')
 def home():
+    return render_template('nav.html')
+
+@app.route('/models')
+def models():
     return render_template('models.html')
+
+@app.route('/team')
+def team():
+    return render_template('teams.html')
 
 if __name__=="__main__":
     app.run(debug=True)
